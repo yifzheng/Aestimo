abea342097d007f38007b626dc3aad7a0e89cc05
Yifeng Zheng <yifeng.zheng43@bcmail.cuny.edu>
2023-08-01 01:59:39 -0400

    added subsciption to PostStore data

--- src/context/PostStore.jsx
+++ src/context/PostStore.jsx
@@ -7,18 +7,25 @@ const PostStore = create( ( set ) => ( {
     setPostOwner: ( postOwnerData ) => set( { postOwner: { ...postOwnerData } } )
 } ) )
 
-/* // check if there is data stored in local storage
+// check if there is data stored in local storage
 const storedPost = localStorage.getItem( 'post' )
+const storedPostOwner = localStorage.getItem( 'postOwner' )
 // check if the stored post is null or undefined
 if ( storedPost !== null ) {
     PostStore.setState( { post: JSON.parse( storedPost ) } )
+}
+if ( storedPostOwner !== null ) {
+    PostStore.setState( { postOwner: JSON.parse( storedPostOwner ) } )
 }
 // subscribe to changes
 PostStore.subscribe(
     ( state ) => { localStorage.setItem( 'post', JSON.stringify( state.post ) ) },
     ( state ) => state.post
 )
- */
+PostStore.subscribe(
+    ( state ) => { localStorage.setItem( 'postOwner', JSON.stringify( state.postOwner ) ) },
+    ( state ) => state.postOwner
+)
 
 
 export default PostStore
